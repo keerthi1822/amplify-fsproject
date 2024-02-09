47700790120d4ba74637370c9dc3ec5c9cf5da46
keerthi1822 <keerthi1822@gmail.com>
2024-02-09 10:10:56 +0100

    updated again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello from Keerthi
+         Hello from Keerthi. Updated
         </p>
       </header>
     </div>
